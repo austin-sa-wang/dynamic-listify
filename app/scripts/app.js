3557c1d4e3316714ea9b3bb5d3641a51affbafa0
Austin SA Wang <austin.sa.wang@gmail.com>
2015-01-06 16:18:07 -0500

    Add filter service; Add unit test for filter service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,12 +13,45 @@ angular
     'LiveSearchCore',
     'liveSearchList'
   ])
-  .controller('srcMarkUpCtrl', function ($scope) {
+  .controller('srcMarkUpCtrl', function ($scope, listFilterFactory) {
     $scope.list1 = {
+      head: 'HEAD',
       data: 'HELLO'
     };
+
     $.get('sample.html', function (response) {
+      var trueSrc = document.createElement('div');
+      trueSrc.innerHTML = response;
+      var listHead = document.createDocumentFragment();
+      var listData = document.createDocumentFragment();
+      listData.appendChild(trueSrc.getElementsByTagName('tbody')[0]);
+      listHead.appendChild(trueSrc.children[0]);
+      $scope.list1.head = listHead.childNodes[0];
+      $scope.list1.listContent = listData.childNodes[0];
       $scope.list1.data = response;
+      console.log($scope.list1.head);
+      console.log($scope.list1.data);
+      listFilterFactory.filterList($scope.list1.listContent, 'a');
     });
+  })
+  .factory('listFilterFactory', function listFilterFactory () {
+    listFilterFactory.filterList = function (_list, filterExpr) {
+      var list = _list.cloneNode(true);
+      var listChildren = list.children;
+      var regex = new RegExp(filterExpr, 'i');
+      var currentNode;
+
+      for (var i = 0; i < listChildren.length; ) {
+        currentNode = listChildren[i];
+        if (!regex.exec(currentNode.innerText)) {
+          list.removeChild(currentNode);
+          i++;
+        }
+      }
+
+      return list;
+    }
+    return listFilterFactory;
   });
+
 
--- test/spec/liveSearchCoreTest.js
+++ test/spec/liveSearchCoreTest.js
@@ -1,13 +1,13 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('live search app', function () {
 
   // load the controller's module
   beforeEach(module('LiveSearchApp'));
-  beforeEach(module('LiveSearchCore'));
 
   var ctrl,
-    scope;
+    scope,
+    listFilterFactory;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -17,7 +17,28 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('unused', function () {
-    expect(true);
+  //beforeEach(function() {
+  //  inject(function($injector) {
+  //    listFilterFactory = $injector.get('listFilterFactory');
+  //  })
+  //});
+
+  beforeEach(inject(function(_listFilterFactory_) {
+    listFilterFactory = _listFilterFactory_;
+  }));
+
+  it('check the existence of listFilterFactory factory', inject(function(listFilterFactory) {
+    expect(listFilterFactory).toBeDefined();
+  }));
+
+  it('should return filtered list', function () {
+    var tbody = document.createElement('tbody');
+    var dataMarkup = '<tr><td>a</td></tr>' +
+      '<tr><td>b</td></tr>' +
+      '<tr><td>c</td></tr>';
+    tbody.innerHTML = dataMarkup;
+
+    var filteredList = listFilterFactory.filterList(tbody, 'b');
+    expect(filteredList.innerText).toEqual('b');
   });
 });

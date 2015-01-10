a59c74794bdff8dbc6074434e3df954a3fce58d2
Austin SA Wang <austin.sa.wang@gmail.com>
2015-01-10 16:11:21 -0500

    Perf: Change to out-of-flow filter logic

--- app/scripts/liveSearchList.js
+++ app/scripts/liveSearchList.js
@@ -29,26 +29,51 @@ angular
         console.log('end append list content');
 
         scope.$watch('ctrl.filterExpr', function () {
-          updateListWithHTMLDisplay();
+          //updateListWithHTMLDisplay();
+          updateListWithHTMLDisplay(listContent);
         });
 
-        function updateListWithHTMLDisplay () {
-          var regex = new RegExp(scope.ctrl.filterExpr, 'i');
-
-          var listChildren = listContent.children;
-          var currentNode;
-
-          // Remove filtered nodes
-          for (var i = 0; i < listChildren.length; i++) {
-            currentNode = listChildren[i];
-            if (!regex.exec(currentNode.innerText)) {
-              currentNode.style.display = 'none';
-            } else {
-              currentNode.style.display = '';
-            }
+        /**
+         * Run callback with the element removed from the DOM (and thus being
+         * out-of-the-flow).  Upon returning, the element will be inserted at its
+         * original position even if callback rises an exception.
+         * @source https://developers.google.com/speed/articles/javascript-dom
+         *
+         * @param {!Element} element The element to be temporarily removed.
+         * @param {function(): T} callback The function to call.
+         * @return {T} Value returned by the callback function.
+         * @template T
+         */
+        function updateDomWithElementRemoved(element, callback) {
+          var parentNode = element.parentNode;
+          var nextSibling = element.nextSibling;
+          parentNode.removeChild(element);
+          try {
+            var retval = callback();
           }
+          finally {
+            parentNode.insertBefore(element, nextSibling);
+          }
+          return retval;
         }
 
+        function updateListWithHTMLDisplay(element) {
+          updateDomWithElementRemoved(element, function() {
+            var regex = new RegExp(scope.ctrl.filterExpr, 'i');
+            var listChildren = element.children;
+            var currentNode;
+
+            // Remove filtered nodes
+            for (var i = 0; i < listChildren.length; i++) {
+              currentNode = listChildren[i];
+              if (!regex.exec(currentNode.innerText)) {
+                currentNode.style.display = 'none';
+              } else {
+                currentNode.style.display = '';
+              }
+            }
+          });
+        }
       }
     }
   });

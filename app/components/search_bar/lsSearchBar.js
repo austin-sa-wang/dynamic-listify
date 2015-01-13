015c21a4cd9bc014e3029262898cbd1fcba0ae9e
Austin SA Wang <austin.sa.wang@gmail.com>
2015-01-13 17:02:24 -0500

    Refactor alert logic; Add no table found alert

--- app/components/search_bar/lsSearchBar.js
+++ app/components/search_bar/lsSearchBar.js
@@ -4,8 +4,9 @@ angular
     'ui.bootstrap'
   ])
 
-  .controller('lsSearchBarCtrl', function ($scope, ListExtractionFactory) {
+  .controller('lsSearchBarCtrl', function ($scope, $timeout, ListExtractionFactory) {
     var ALERT_MSG = 'Try again. Extraction failed.';
+    var NO_TABLE_ALERT_MSG = 'No table found on target page. If there IS a table, then the table implementation is not supported. This app finds tables by the HTML <table> element.';
     var SEVERE_ALERT_MSG = 'Try a different web page. The app is unable to retrieve source data due to cross-origin resource sharing restrictions.';
 
     this.srcUrl = 'http://bindingofisaacrebirth.gamepedia.com/Items';
@@ -18,24 +19,38 @@ angular
     var _alert = this.alert;
 
     var showAlert = function() {
-      if (_alert.count == 2) {
+      if (_alert.count > 1) {
         _alert.msg = SEVERE_ALERT_MSG;
         _alert.type = 'danger';
+      } else {
+        _alert.msg = ALERT_MSG;
+        _alert.type = '';
       }
       _alert.show = true;
       _alert.count++;
     };
 
-    var resetAlert = function() {
-      _alert.show = false;
+    var showNoTableFoundAlert = function () {
+      _alert.show = true;
       _alert.type = '';
-      _alert.msg = ALERT_MSG;
+      _alert.msg = NO_TABLE_ALERT_MSG;
       _alert.count = 0;
     };
 
+    ListExtractionFactory.listen(function (event, data) {
+      if (data === 0) {
+        $timeout(function() {
+          showNoTableFoundAlert();
+        });
+      }
+    });
+
     this.getTables = function () {
+      _alert.show = false;
       ListExtractionFactory.extract(this.srcUrl)
-        .success(resetAlert)
+        .success(function() {
+          _alert.count = 0;
+        })
         .error(showAlert);
     };
 

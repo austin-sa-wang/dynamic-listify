185f2286cd72fd8695c21d2c7605161bbaf801aa
Austin SA Wang <austin.sa.wang@gmail.com>
2015-01-14 11:34:59 -0500

    Add Corsproxy note; Add preset url; Fix Extract Tables button; Resolve #14

--- app/components/search_bar/lsSearchBar.js
+++ app/components/search_bar/lsSearchBar.js
@@ -8,8 +8,9 @@ angular
     var ALERT_MSG = 'Try again. Extraction failed.';
     var NO_TABLE_ALERT_MSG = 'No table found on target page. If there IS a table, then the table implementation is not supported. This app finds tables by the HTML <table> element.';
     var SEVERE_ALERT_MSG = 'Try a different web page. The app is unable to retrieve source data due to cross-origin resource sharing restrictions.';
+    var PROCESSING_MSG = 'Processing...';
 
-    this.srcUrl = 'http://bindingofisaacrebirth.gamepedia.com/Items';
+    this.srcUrl = '';
     this.alert = {
       show: false,
       type: '',
@@ -37,6 +38,12 @@ angular
       _alert.count = 0;
     };
 
+    var showProcessingAlert = function () {
+      _alert.show = true;
+      _alert.type = '';
+      _alert.msg = PROCESSING_MSG;
+    };
+
     ListExtractionFactory.listen(function (event, data) {
       if (data === 0) {
         $timeout(function() {
@@ -46,12 +53,18 @@ angular
     });
 
     this.getTables = function () {
-      _alert.show = false;
+      showProcessingAlert();
       ListExtractionFactory.extract(this.srcUrl)
         .success(function() {
+          _alert.show = false;
           _alert.count = 0;
         })
         .error(showAlert);
+    };
+
+    this.setUrl = function(url) {
+      this.srcUrl = url;
+      this.getTables();
     };
 
   })

a66d625ba024ac1f152cc42ac88c108e6759b06e
Austin SA Wang <austin.sa.wang@gmail.com>
2015-01-21 17:28:09 -0500

    dist

--- components/dependencyUnavailableDirective.js
+++ components/dependencyUnavailableDirective.js
@@ -7,10 +7,8 @@ angular
     var DEPENDENCY_URL = 'http://www.whateverorigin.org/';
     return {
       compile: function (tElem) {
-        console.log('dependencyUnavailable');
         var script = document.body.appendChild(document.createElement('script'));
         script.onerror = function () {
-          console.log('unavailable');
           tElem.removeClass('hidden');
         };
         script.src = DEPENDENCY_URL;
--- components/search_bar/lsSearchBar.js
+++ components/search_bar/lsSearchBar.js
@@ -10,8 +10,9 @@ angular
     //TODO: Refactor - Extract status UI out of lsSearchBarCtrl
     var NO_TABLE_ALERT_MSG = 'No table found on target page. If there IS a table, then the table implementation is not supported. This app finds tables by the HTML <table> element.';
     var PROCESSING_MSG = 'Processing...';
-    var UNRESPONSIVE_MSG = 'Target site is unresponsive. Try again or try a different site.';
-    var EMPTY_URL_MSG = 'Empty URL';
+    var UNRESPONSIVE_MSG = 'Target site is unresponsive. Try a different site.';
+    var FAST_FAIL_MSG = 'There\'s a slight hiccup with the request. It should work within 3 tries. (The issue is being resolved)';
+    var NOT_HTTP_MSG = 'Invalid URL. Note: The url should be preceded with http(s)://';
 
     this.srcUrl = '';
     this.alert = {
@@ -47,28 +48,31 @@ angular
     });
 
     this.getTables = function () {
-      // url empty
-      if (this.srcUrl === '') {
-        _alert.error(EMPTY_URL_MSG);
+      // Check url validity. Only http(s) protocol is allowed
+      var regex = /^https?:\/\/.+/;
+      if ( !regex.test(this.srcUrl) ) {
+        _alert.error(NOT_HTTP_MSG);
         return;
       }
 
-      /* HACK: url missing protocol
-       * Potential problem: protocol could be https
+      /* Hack: get around intermittent failure when the http request fails immediately (possibly caused by whatever origin)
+       * (cannot differentiate between this and timeout because both callback arguments are empty)
+       * Cancel the timer in the extract error callback. If too soon, throw retry message.
        */
-      var regex = /^http/;
-      if ( !regex.test(this.srcUrl) ) {
-        this.srcUrl = 'http://' + this.srcUrl;
-      }
+      var timer = $timeout(function(){}, 1000);
 
       _alert.warning(PROCESSING_MSG);
-
       ListExtractionFactory.extract(this.srcUrl)
         .success(function() {
             _alert.reset();
         })
         .error(function() {
+          // HACK: Part of the Hack described above
+          if ($timeout.cancel(timer)) {
+            _alert.error(FAST_FAIL_MSG);
+          } else {
             _alert.error(UNRESPONSIVE_MSG);
+          }
         });
     };
 
